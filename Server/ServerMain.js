2a5d5bf17953e55fd77ff0674b03eb3b90cf5d3c
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2024-09-03 20:49:19 -0500

    Basic TCP nodejs server now works with TestConnection java file to test

--- Server/ServerMain.js
+++ Server/ServerMain.js
@@ -1,8 +1,38 @@
 const net = require('net');
-const port = 443;
-const host = 'localhost';
+const https = require('https');
 
+const fs = require('fs');
+const path = require('path');
+/*
+const express = require('express');
+const app = express();
+const Port_HTTPS = 443;
+
+app.use(express.static('../Website'));
+
+*/
+const Port_TCP = 4000;
+const host = "127.0.0.1";
 const server = net.createServer();
-server.listen(port, host, () => {
-    console.log('TCP Server is running on port ' + port + '.');
+
+var sockets = [];
+server.listen(Port_TCP, host, () => {
+    console.log('TCP Server is running on port ' + Port_TCP + '.');
+});
+server.on('connection', function(socket) {
+    sockets.push(socket);
+    console.log('Connected: ' + socket.remoteAddress + ':' + socket.remotePort);
+
+    socket.on('end',function(){
+        console.log('Disconnected: ' + socket.remoteAddress + ':' + socket.remotePort);
+        sockets.splice(sockets.indexOf(socket), 1);
+    });
+
+    socket.on('error', function(err){
+        console.log('Error: ' + err);
+    });
+
+});
+server.on('error', (err) => {
+    console.log('Error: ', err);
 });
---
+++ Server/TestConnection.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class TestConnection {
+    
+    public static void main(String[] args) {
+        try {
+            // Create a socket to connect to the server
+            Socket socket = new Socket("127.0.0.1", 4000);
+
+            System.out.println("Connected to the server");
+
+            // Close the socket
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
