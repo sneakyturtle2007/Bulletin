56d85a2772b54fde4b5bb6a0827647b930eb8e01
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2024-09-21 23:01:23 -0500

    commiting to sync

--- Server/Database.js
+++ Server/Database.js
@@ -4,14 +4,28 @@ let db = new sql.Database('./database.db',  (err) => {
     if (err) {
         console.error(err.message);
     }
-    console.log('Connected to the in-memory SQlite database.');
+    console.log('Connected to the SQlite database.');
 });
+
+function ShowTable(){
+
+}
+
 /*db.serialize(() =>{
     db.run('CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT NOT NULL, email TEXT NOT NULL DEFAULT "unknown", password TEXT NOT NULL)');
 });*/
+db.serialize(() =>{
+   db.each('SELECT name FROM sqlite_master WHERE type="table";', (err,tables) => {
+       if(err){
+          console.log(err.message);
+       }
+       console.log(tables);
+   })
+});
+
 db.close((err) => {
     if (err) {
         console.error(err.message);
     }
     console.log('Close the database connection.');
-});
+}); 
