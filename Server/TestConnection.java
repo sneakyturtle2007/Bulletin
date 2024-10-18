05958676c870f98c70140a9e698ffccf1dee152e
Luis Blake <103067582+sneakyturtle2007@users.noreply.github.com>
2024-10-17 22:35:09 -0500

    Added a little bit of documentation to the API call for the TCP part of the server.

--- Server/TestConnection.java
+++ Server/TestConnection.java
@@ -1,44 +1,44 @@
-import java.util.*;
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class TestConnection {
 
-    public static void main(String[] args) {
-        InputStreamReader inputStreamReader;
-        OutputStreamWriter outputStreamWriter;
-        BufferedReader bufferedReader;
-        BufferedWriter bufferedWriter;
-        System.out.println("test");
-        try {
-            // Create a socket to connect to the server
-            Socket socket = new Socket("127.0.0.1", 22);
-            inputStreamReader = new InputStreamReader(socket.getInputStream());
-            outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
-            bufferedReader = new BufferedReader(inputStreamReader);
-            bufferedWriter = new BufferedWriter(outputStreamWriter);
-            System.out.println("Connected to the server");
-            bufferedWriter.newLine();
-            bufferedWriter.flush();
-            bufferedWriter.write("createuser test email@gmail.com test2");
-            bufferedWriter.newLine();
-            bufferedWriter.flush();
-            System.out.println("sent message to server");
-            String message = "";
-            System.out.println("Server: " + message);
-            while(true){
-                if(bufferedReader.ready()){
-                    message = bufferedReader.readLine();
-                    System.out.println("Server: " + message);
-                    if(message.equals("User created")){
-                        break;
-                    }
-                }
-            }
-            // Close the socket
-            socket.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+  public static void main(String[] args) {
+    InputStreamReader inputStreamReader;
+    OutputStreamWriter outputStreamWriter;
+    BufferedReader bufferedReader;
+    BufferedWriter bufferedWriter;
+    System.out.println("test");
+    try {
+      // Create a socket to connect to the server
+      Socket socket = new Socket("127.0.0.1", 22);
+      inputStreamReader = new InputStreamReader(socket.getInputStream());
+      outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+      bufferedReader = new BufferedReader(inputStreamReader);
+      bufferedWriter = new BufferedWriter(outputStreamWriter);
+      System.out.println("Connected to the server");
+      bufferedWriter.newLine();
+      bufferedWriter.flush();
+      bufferedWriter.write("createuser test email@gmail.com test2");
+      bufferedWriter.newLine();
+      bufferedWriter.flush();
+      System.out.println("sent message to server");
+      String message = "";
+      System.out.println("Server: " + message);
+      while (true) {
+        if (bufferedReader.ready()) {
+          message = bufferedReader.readLine();
+          System.out.println("Server: " + message);
+          if (message.equals("User created")) {
+            break;
+          }
         }
+      }
+      // Close the socket
+      socket.close();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 }
