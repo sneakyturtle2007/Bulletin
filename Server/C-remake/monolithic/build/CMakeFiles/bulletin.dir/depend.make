37018b75881b8d1d12f1a5acee6160517c74799f
Luis Blake <luis.blake01@outlook.com>
2025-08-08 02:34:24 -0500

    Fixing git ignore

--- Server/C-remake/monolithic/build/CMakeFiles/bulletin.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for bulletin.
-# This may be replaced when dependencies are built.
--- Server/C-remake/monolithic/build/external/json-c/doc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luis/github/Bulletin/Server/C-remake/monolithic/external/json-c/doc
-# Build directory: /home/luis/github/Bulletin/Server/C-remake/monolithic/build/external/json-c/doc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
